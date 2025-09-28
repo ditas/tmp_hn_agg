d7675a3f50496d8fb0ca7cd9b289a3c6ad30fba6
Uokno Tyuoi <ditasandditas@gmail.com>
2025-09-28 13:48:29 +0100

    Fix applications starting conflict

--- test/hn_aggregator_SUITE.erl
+++ test/hn_aggregator_SUITE.erl
@@ -12,8 +12,6 @@
     all/0,
     init_per_suite/1,
     end_per_suite/1
-    % init_per_testcase/2,
-    % end_per_testcase/2
 ]).
 
 -export([
@@ -25,12 +23,20 @@ all() -> [
 ].
 
 init_per_suite(Config) ->
+    % timer:sleep(5000),
+    % ok = application:stop(hn_aggregator),
+
     Env = [{hn_aggregator, ct:get_config(hn_aggregator)}],
     [application:set_env(hn_aggregator, Key, Val) || {Key, Val} <- proplists:get_value(hn_aggregator, Env)],
 
+    ok = application:start(sasl),
+    ok = application:start(jsone),
+    ok = application:start(observer),
+
     ok = hn_api_mock:start(?HN_API_MOCK_PORT), %% Start the mock HN API server
 
-    % _ = application:ensure_all_started(hn_aggregator),
+    {ok, _} = application:ensure_all_started(hn_aggregator),
+
     timer:sleep(3000),
     Ping = os:cmd("ping http://localhost:9999/stories"),
     ct:pal("Ping ~p", [Ping]),
@@ -40,27 +46,13 @@ end_per_suite(_Config) ->
     ok = application:stop(hn_aggregator),
     ok.
 
-% init_per_testcase(poll_stories_success, Config) ->
-
-%     %% TODO: fix me
-%     hn_api_mock_state_table = ets:new(hn_api_mock_state_table, [named_table, public]),
-
-%     ok = hn_api_mock:set_response(topstories, ?STORIES_IDS),
-%     ok = hn_api_mock:set_response(items, ?STORIES),
-%     Config;
-% init_per_testcase(_TestCase, Config) ->
-%     Config.
-
-% end_per_testcase(_TestCase, _Config) ->
-%     ok.
-
 poll_stories_success(_Config) ->
     timer:sleep(30000),
     % {ok, {{_, 200, _}, _, Resp}}
     Resp
     = httpc:request(
         get,
-        {?HN_AGGREGATOR ++ "/456", []},
+        {?HN_AGGREGATOR ++ "456", []},
         [],
         []
     ),
--- test/hn_api_mock.erl
+++ test/hn_api_mock.erl
@@ -2,14 +2,8 @@
 
 -export([
     start/1,
-    set_response/2,
-    set_delay/1,
-    set_error/1,
-    reset_state/0,
     stop/0
 ]).
-
--define(HN_API_MOCK_STATE_TABLE, hn_api_mock_state_table).
 
 start(HNApiMockPort) ->
     {ok, _} = application:ensure_all_started([ranch, cowboy]),
@@ -21,7 +15,7 @@ start(HNApiMockPort) ->
         ]}
     ]),
     {ok, _} = cowboy:start_clear(
-        http_listener,
+        http_mock,
         [{port, HNApiMockPort}],
         #{env => #{dispatch => Dispatch}}
     ),
@@ -32,19 +26,3 @@ start(HNApiMockPort) ->
 
 stop() ->
     cowboy:stop_listener(http_listener).
-
-set_response(Type, Response) ->
-    true = ets:insert(?HN_API_MOCK_STATE_TABLE, {Type, Response}),
-    ok.
-
-set_delay(DelayMS) ->
-    true = ets:insert(?HN_API_MOCK_STATE_TABLE, {delay, DelayMS}),
-    ok.
-
-set_error(Error) ->
-    true = ets:insert(?HN_API_MOCK_STATE_TABLE, {error, Error}),
-    ok.
-
-reset_state() ->
-    true = ets:delete_all_objects(?HN_API_MOCK_STATE_TABLE),
-    ok.
